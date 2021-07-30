c9a209a96ae5673ace25c70788800eff65ac7599
robertoAureli <r.aureli@protonmail.com>
2021-07-30 23:10:04 +0200

    Funzione che restituisce il prezzo del panino

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
 let btn_GenerateBurger = document.getElementById("btn-generate-burger");
 btn_GenerateBurger.addEventListener('click',
     function () {
-        // let burgerName = document.getElementById("input-burger-name").value;
-        // let chesse = document.getElementById("chesse");
-        // let eggs = document.getElementById("eggs");
-        // let mustard = document.getElementById("mustard");
-        // let tomato = document.getElementById("tomato").value;
-        // let lettuce = document.getElementById("lettuce");
-        // let ketchup = document.getElementById("ketchup");
-        // console.log(`Name: ${burgerName}  tomato: ${tomato}`);
         let user_burger = burgerComposition();
-        console.log(user_burger);
+        //console.log(user_burger);
+        calculatePriceBurger(user_burger)
     })
 
 /*  
@@ -24,14 +17,54 @@ btn_GenerateBurger.addEventListener('click',
 function burgerComposition() {
     var user_burger = {
         burgerName: document.getElementById("input-burger-name").value,
-        chesse: document.getElementById("chesse").value,
-        mustard: document.getElementById("mustard").value,
-        tomato : document.getElementById("tomato").value,
-        lettuce : document.getElementById("lettuce").value,
-        ketchup : document.getElementById("ketchup").value,
-        coupon : document.getElementById("coupon").value
+        chesse: document.getElementById("chesse").checked,
+        mustard: document.getElementById("mustard").checked,
+        tomato: document.getElementById("tomato").checked,
+        lettuce: document.getElementById("lettuce").checked,
+        ketchup: document.getElementById("ketchup").checked,
+        coupon: document.getElementById("coupon").value
 
     }
 
     return user_burger;
+}
+
+/*  
+    =======================================================================================================
+        2.                              Restituisce il prezzo del panino
+    =======================================================================================================
+    
+*/
+
+function calculatePriceBurger(user_burger) {
+    var priceAddonBurger = {
+        chesse: 40,
+        mustard: 10,
+        tomato: 20,
+        lettuce: 20,
+        ketchup: 10,
+    }
+
+    var priceBaseBurger = 320;
+
+    // Ritorna gli ingredienti selezionati
+    var keys = Object.keys(user_burger);
+    var ingredients = keys.filter(function (key) {
+        return user_burger[key]
+    });
+    if (ingredients[0] == "burgerName") {
+        ingredients.shift();
+    }
+    console.log(ingredients);
+    let price = 0;
+    ingredients.forEach( function(ingredient){
+        let index = ingredient.toString();
+        price  += priceAddonBurger[ingredient];
+        return price;
+        
+        
+    });
+    price = (price + priceBaseBurger) / 100;
+    return price;
+
 }
