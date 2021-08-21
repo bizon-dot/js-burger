fe079e7ef22326f628dadce7a8ae830b50be04b6
robertoAureli <r.aureli@protonmail.com>
2021-08-21 12:03:39 +0200

    Funzione codice sconto

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ btn_GenerateBurger.addEventListener('click',
         // e il prezzo
         let user_burger = burgerComposition();
         // Stampo nel DOM il banner con il prezzo 
-        console.log(user_burger);
         let banner_price = bannerPrice(user_burger);
 
     })
@@ -38,21 +37,20 @@ function burgerComposition() {
         }
     }
 
-    // ?! TO FIX
     // CouponCode
-    //let couponCode = document.getElementById("coupon").value;
+    let couponCode = document.getElementById("coupon").value;
 
     // Controllo che il codice coupoun sia valido nel caso salvo il flag true
-    // if (isPalindrome(couponCode)) {
-    //    user_burger["coupon"] = true;
-    // }
+    if (isCoupon(couponCode)) {
+       user_burger["coupon"] = true;
+    }
 
     // Calcolo e e aggiungo il prezzo del panino all'oggetto tenendo conto dell'
     //  eventuale sconto
     let price = calculatePriceBurger(user_burger);
     
-    user_burger["price"] = (price);
-    
+    user_burger["price"] = price;
+    console.log(user_burger);
     return user_burger;
 }
 
@@ -76,24 +74,22 @@ function calculatePriceBurger(user_burger) {
 
     // Calcolo il prezzo in base agli ingredienti contenuti in user_burger
     let price = 0;
-    //console.log(typeof(priceAddonBurger.tomato))
+
     for (let ingredient in user_burger) {
         // Escludo tutte le proprietà non numeriche dell'oggetto dalla somma 
-        if (!(isNaN(user_burger[ingredient]))) {
-             price +=  (priceAddonBurger[ingredient]);
-             
-
+        if (!(isNaN(user_burger[ingredient])) && !(user_burger[ingredient])) {
+            price +=  (priceAddonBurger[ingredient]);
+            
         }
 
     }
-    price = (price + parseInt(priceAddonBurger.priceBaseBurger)) / 100;
-    
-    // ?! TO FIX
+    price = (price + priceAddonBurger.priceBaseBurger)/ 100;
     // Nel caso user_burger.coupon === true applico uno sconto del 20&
-    // if (user_burger.coupon) {
-    //     price = price * 0.8;
-    // }
-    
+    if (user_burger.coupon ) {
+      price *= 0.8;
+      price = price.toFixed(1);
+        
+    }
     return price;
 
 }
@@ -108,22 +104,20 @@ function calculatePriceBurger(user_burger) {
 function bannerPrice(user_burger) {
  
     let templateBox = document.getElementById("box-price");
-    templateBox.innerHTML = `Price: ${user_burger.price}`;
+    templateBox.innerHTML = `Price: ${user_burger.price} $`;
+    templateBox.classList.remove("ghost");
     return true;
 }
 
 /*  
     =======================================================================================================
-        4.                              Torna true se il discount code è palindromo
+        4.                             Controllo se il codice è incluso nell'array coupon
     =======================================================================================================
     
 */
-// Strip i caratteri alfanumerici e rendo tutto minuscolo
-const clean = (str) => str.toString().toLowerCase().replace(/[\W_]/g, '');
-function isPalindrome(str) {
-    var clearStr = clean(str);
-    var reverseStr = clearStr.split('').reverse().join('');
-    return clearStr === reverseStr;
+function isCoupon(code){
+    let validCoupon = ["A112358", "B112358", "C112358", "D112358"];
+    return validCoupon.includes(code) ? true : false;
 }
 
 
